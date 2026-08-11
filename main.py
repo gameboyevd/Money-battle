4513a9d3a43cda3ef74cbc4c8426821046ee092e
gameboyevd <87015423+gameboyevd@users.noreply.github.com>
2026-08-11 19:29:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -691,7 +691,7 @@ class MainView(discord.ui.View):
 
                 await interaction.response.send_message(
                     f"⚠️ 이미 게임에 참가되어 있습니다!\n\n"
-                    f"👥 현재 참가자: **{count}명**",
+                    f"👥 현재 참가자: **{count}명}**",
                     ephemeral=True
                 )
 
